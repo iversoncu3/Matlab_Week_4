a79a8a4a4cf15a95ad7ea0f4004acfac68db1272
Baran AKBIYIK <baranbaskent1@gmail.com>
2016-10-17 12:32:22 +0300

    Odev

---
+++ Untitled5.m
@@ -0,0 +1,7 @@
+function [ x ] = Untitled5(  x1,y1,x2,y2 )
+%UNTÝTLED5 Summary of this function goes here
+%   Detailed explanation goes here
+x=sqrt((x2-x1).^2+(y2-y1).^2);
+
+end
+
---
+++ fact.m
@@ -0,0 +1,7 @@
+ function x = fact(n)
+    if n<=1
+       x = 1;    
+    else
+       x = n .* fact(n-1) ;
+    end
+ end
